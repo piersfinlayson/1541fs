2ef3b59310989b559782cea101cfdb323dee5db5
Piers Finlayson <piers@piers.rocks>
2024-12-11 20:27:19 +0000

    clean up

--- fuse/cbmdummy.c
+++ fuse/cbmdummy.c
@@ -5,22 +5,20 @@
 const struct dummy_files dummy_files[] =
 {
     {
-        .filename = PATH_FORCE_DISK_REREAD,
-        .contents =
-            "To format a disk write the new disk name followed by the ID to this file.\n"
-            "The disk name can be a maximum of 16 characters and the ID two digits.\n"
-            "Separate the two with a comma.  For example:\n"
-            "  echo \"my new disk,01\" > ./" PATH_FORMAT_DISK "\n",
+        PATH_FORCE_DISK_REREAD,
+        "To format a disk write the new disk name followed by the ID to this file.\n"
+        "The disk name can be a maximum of 16 characters and the ID two digits.\n"
+        "Separate the two with a comma.  For example:\n"
+        "  echo \"my new disk,01\" > ./" PATH_FORMAT_DISK "\n",
     },
     {
-        .filename = PATH_FORMAT_DISK,
-        .contents = 
-            "To force a disk re-read, write anything to this file.  For example:\n"
-            "  echo \"1\" > ./" PATH_FORCE_DISK_REREAD "\n",
+        PATH_FORMAT_DISK,
+        "To force a disk re-read, write anything to this file.  For example:\n"
+        "  echo \"1\" > ./" PATH_FORCE_DISK_REREAD "\n",
     },
     {
-        .filename = NULL,
-        .contents = NULL,
+        NULL,
+        NULL,
     },
 };
 
